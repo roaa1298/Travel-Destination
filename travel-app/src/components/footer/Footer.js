cded0f2d81002f924bedabbf3b70a54181a5ab7f
roaa1998 <roaabhailat4@gmail.com>
2022-02-14 01:29:44 +0200

    travel app

---
+++ travel-app/src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+import './Footer.css';
+
+function Footer(){
+    return(
+        <>
+        <div className="foot">
+        <a href="http://www.facebook.com" className='face'>facebook</a>
+        <a href="http://www.instagram.com" className='insta'>instagram</a>
+        <p className='copy'>Copyright &copy; All rights reserved</p>
+        </div>
+        </>
+    );
+}
+
+export default Footer;
---
+++ travel-app/src/components/header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header(){
+    return(
+        <>
+        <h1 className="head">Travel Destination</h1>
+        </>
+    );
+}
+
+export default Header;
